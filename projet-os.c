510d47cdc75a62549a261df1a19c6aa2fb07c647
Karitchi <karichikarichikarichi@gmail.com>
2021-10-06 12:55:08 +0200

    Essai implémentation fork()

--- projet-os.c
+++ projet-os.c
@@ -4,19 +4,16 @@
 
 void main(void)
 {
+    int pId = 0;
     int cars[] = {1, 2, 3, 4, 5, 6, 7};
     float sector[3][7];
 
     srand(time(NULL));
 
     // Generation des temps de chaque voiture pour chaque secteur
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < sizeof(cars) / 4; i++)
     {
-        for (int j = 0; j < sizeof(cars) / 4; j++)
-        {
-            sector[i][j] = ((rand() / (float)(RAND_MAX)) * 9.0) + 35;
-            printf("%f ", sector[i][j]);
-        }
-        printf("\n");
+        pId = fork();
     }
+    printf("%d /n", pId);
 }
