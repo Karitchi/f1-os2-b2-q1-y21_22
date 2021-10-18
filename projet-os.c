d61cb56d3a996162bc4369ee88267019b2974ce4
Karitchi <karitchikaritchikaritchi@gmail.com>
2021-10-18 17:18:42 +0200

    test

--- projet-os.c
+++ projet-os.c
@@ -3,45 +3,12 @@
 #include <unistd.h>
 #include <time.h>
 
-typedef struct car
+typedef struct cars
 {
     float sector1;
     float sector2;
     float sector3;
-} structCar;
-
-void generateChilds(int numberOfCars, int *pId)
-{
-    for (int i = 0; i < numberOfCars; i++)
-    {
-        *pId = fork();
-        if (!*pId)
-        {
-            break;
-        }
-    }
-}
-
-void generateRandomTimes(structCar *cars, int *positionInCars)
-{
-    wait();
-    srand(time(NULL));
-
-    cars[*positionInCars].sector1 = ((35 - 45) * ((float)rand() / RAND_MAX)) + 35;
-    cars[*positionInCars].sector2 = ((30 - 40) * ((float)rand() / RAND_MAX)) + 30;
-    cars[*positionInCars].sector3 = ((32 - 48) * ((float)rand() / RAND_MAX)) + 32;
-
-    *positionInCars++;
-}
-
-void printSectors(structCar *cars, int numberOfCars)
-{
-    for (int i = 0; i < numberOfCars; i++)
-    {
-        printf("voiture %d :\n\t-secteur1 : %f\n\t-secteur2 : %f\n\t-secteur3 : %f\n\n",
-               i + 1, cars[i].sector1, cars[i].sector2, cars[i].sector3);
-    }
-}
+} cars;
 
 void main(void)
 {
@@ -49,7 +16,8 @@ void main(void)
     int numberOfCars = sizeof(carsNumber) / 4;
     int positionInCars = 0;
     int pId = 0;
-    structCar cars[numberOfCars];
+
+    cars[numberOfCars];
 
     generateChilds(numberOfCars, &pId);
     if (!pId)
