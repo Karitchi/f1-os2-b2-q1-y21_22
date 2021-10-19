40b79dd395b04c1fbab58d7f3c3aeb6e3f89d57f
Karitchi <karitchikaritchikaritchi@gmail.com>
2021-10-19 19:05:00 +0200

    Merge branch 'test' of https://github.com/Karitchi/Projet-os into test

--- projet-os.c
+++ projet-os.c
@@ -4,66 +4,67 @@
 #include <time.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include "structure.h"
 
-typedef struct cars
+void generateChilds(int *pId, int *childId, int numberOfCars)
 {
-    float sector[3];
-} cars;
-
-typedef struct shareMemory
-{
-    cars car[5];
-} shareMemory;
-
-void generateChilds(int *pId)
-{
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < numberOfCars; i++)
     {
         *pId = fork();
 
         if (!*pId)
         {
+            *childId = i;
             break;
         }
     }
 }
 
-int generateRandomNumber(int *seed)
+float generateRandomNumber(shareMemory *shareMemory)
 {
-    srand(*seed);
-    ++*seed;
-    return (rand() % 9 + 40);
+    srand(shareMemory->seed);
+    shareMemory->seed++;
+    return (float)rand() / (float)(RAND_MAX)*9 + 40;
 }
 
-void generateTimesP1(shareMemory *shareMemory, int *seed)
+void generateTimesP1(shareMemory *shareMemory, int childId)
 {
     for (int i = 0; i < 3; i++)
     {
-        shareMemory->car[0].sector[i] = generateRandomNumber(seed);
+        shareMemory->car[childId].sector[i] = generateRandomNumber(shareMemory);
     }
 }
 
 void main(void)
 {
-    int carsNumber[] = {1, 2, 3, 4, 5};
-    int seed = time(NULL);
     int shmId;
     int pId;
+    int childId;
+    int carsNumber[] = {44, 77, 11, 33, 3, 4, 5, 18, 14, 31, 16, 55, 10, 22, 7, 99, 9, 47, 6, 63};
+    int numberOfCars = sizeof(carsNumber) / 4;
 
     shareMemory *shareMemory;
 
-    shmId = shmget(1024, sizeof(*shareMemory), IPC_CREAT | 0666);
+    shmId = shmget(1027, sizeof(*shareMemory), IPC_CREAT | 0666);
     shareMemory = shmat(shmId, NULL, 0);
 
-    generateChilds(&pId);
+    //Stockage du seed dans la memoire partagee pour permettre aux fils de la modifier.
+    shareMemory->seed = time(NULL);
+
+    generateChilds(&pId, &childId, numberOfCars);
+
     if (!pId)
     {
-        generateTimesP1(shareMemory, &seed);
+        generateTimesP1(shareMemory, childId);
     }
     else
     {
-        printf("%f\n", shareMemory->car[0].sector[0]);
-        printf("%f\n", shareMemory->car[0].sector[1]);
-        printf("%f\n", shareMemory->car[0].sector[2]);
+        for (int i = 0; i < numberOfCars; i++)
+        {
+            printf("%.3f\n", shareMemory->car[i].sector[0]);
+            printf("%.3f\n", shareMemory->car[i].sector[1]);
+            printf("%.3f\n", shareMemory->car[i].sector[2]);
+            printf("\n\n\n");
+        }
     }
 }
---
+++ structure.h
@@ -0,0 +1,9 @@
+typedef struct shareMemory
+{
+    struct cars
+    {
+        float sector[3];
+    } car[3];
+
+    int seed;
+} shareMemory;
