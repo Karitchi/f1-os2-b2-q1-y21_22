408b463514504e6e04fe405505b4e2c734dc6aca
Karitchi <karichikarichikarichi@gmail.com>
2021-10-19 17:15:57 +0200

    aleatoire

--- projet-os.c
+++ projet-os.c
@@ -2,31 +2,68 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 typedef struct cars
 {
-    float sector1;
-    float sector2;
-    float sector3;
+    float sector[3];
 } cars;
+
+typedef struct shareMemory
+{
+    cars car[5];
+} shareMemory;
+
+void generateChilds(int *pId)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        *pId = fork();
+
+        if (!*pId)
+        {
+            break;
+        }
+    }
+}
+
+int generateRandomNumber(int *seed)
+{
+    srand(*seed);
+    ++*seed;
+    return (rand() % 9 + 40);
+}
+
+void generateTimesP1(shareMemory *shareMemory, int *seed)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        shareMemory->car[0].sector[i] = generateRandomNumber(seed);
+    }
+}
 
 void main(void)
 {
     int carsNumber[] = {1, 2, 3, 4, 5};
-    int numberOfCars = sizeof(carsNumber) / 4;
-    int positionInCars = 0;
-    int pId = 0;
+    int seed = time(NULL);
+    int shmId;
+    int pId;
 
-    cars[numberOfCars];
+    shareMemory *shareMemory;
 
-    generateChilds(numberOfCars, &pId);
+    shmId = shmget(1024, sizeof(*shareMemory), IPC_CREAT | 0666);
+    shareMemory = shmat(shmId, NULL, 0);
+
+    generateChilds(&pId);
     if (!pId)
     {
-        generateRandomTimes(cars, &positionInCars);
+        generateTimesP1(shareMemory, &seed);
     }
-
-    if (pId)
+    else
     {
-        printSectors(cars, numberOfCars);
+        printf("%f\n", shareMemory->car[0].sector[0]);
+        printf("%f\n", shareMemory->car[0].sector[1]);
+        printf("%f\n", shareMemory->car[0].sector[2]);
     }
 }
