27709078477d9ad70e1e235171c29bedbb0c50ca
Karitchi <Karichikarichikarichi@gmail.com>
2021-10-29 13:34:55 +0200

    ajout de fonction dans mainprocess.h

--- mainProcess.h
+++ mainProcess.h
@@ -8,9 +8,13 @@
 
 void *createSharedMemory(sharedMemory *sharedMemory, int shmId, int key);
 
+void initializeGPRelativeData(sharedMemory *sharedMemory, int *carNumbers);
+
+void initializeFinalRace(sharedMemory *sharedMemory);
+
 void choseRace(int *chosenRace, float *timeOfRace);
 
-void initializeGPRelativeData(sharedMemory *sharedMemory, int *carNumbers);
+void calculateNumberOfLaps(int *trackLength, int *numberOfLaps, const int RACE_LENGTH);
 
 void initializeRaceRelativeData(sharedMemory *sharedMemory);
 
@@ -18,10 +22,24 @@ void generateChilds(int *pId, int *childId);
 
 void initializeBestSectors(sharedMemory *sharedMemory);
 
+void addTimesToCars(sharedMemory *sharedMemory);
+
 void findBestSectors(sharedMemory *sharedMemory);
 
 void sortCarsByBestLap(sharedMemory *sharedMemory);
 
+void placeOutCarsLastPlace(sharedMemory *sharedMemory);
+
+void sortCarsByAvgSpeed(sharedMemory *sharedMemory);
+
+void calculateIntervalBestLap(sharedMemory *sharedMemory);
+
+void calculateIntervalAvgSpeed(sharedMemory *sharedMemory);
+
 int display(sharedMemory *sharedMemory);
+
+void displayWithoutColors(sharedMemory *sharedMemory);
+
+int writeDisplayToFile(sharedMemory *sharedMemory, int chosenRace);
 
 void eliminate5LastCars(sharedMemory *sharedMemory);
